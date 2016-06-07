93664f8e995edd0fdd9265606fe0832da876d721
Massimiliano Marcon <me@marcon.me>
2016-06-07 21:05:21 +0200

    Adds alias to sublime text 3

--- aliases.zsh
+++ aliases.zsh
@@ -2,3 +2,6 @@ alias zshconfig="subl ~/.zshrc"
 alias ohmyzsh="subl ~/.oh-my-zsh"
 alias zshreload="source ~/.zshrc"
 alias v="vagrant"
+
+# Sublime Text 3
+alias subl="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
