82b49d4b33794ccc61247f4d51833a2db183bc16
Massimiliano Marcon <me@marcon.me>
2016-10-06 14:03:42 +0200

    Extracts more stuff from .zsh

--- aliases.zsh
+++
@@ -1,7 +0,0 @@
-alias zshconfig="subl ~/.zshrc"
-alias ohmyzsh="subl ~/.oh-my-zsh"
-alias zshreload="source ~/.zshrc"
-alias v="vagrant"
-
-# Sublime Text 3
-alias subl="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
---
+++ overrides.zsh
@@ -0,0 +1,18 @@
+# Aliases
+alias zshconfig="subl ~/.zshrc"
+alias ohmyzsh="subl ~/.oh-my-zsh"
+alias zshreload="source ~/.zshrc"
+alias v="vagrant"
+
+alias github='git -c user.name="Massimiliano Marcon" -c user.email="me@marcon.me"'
+
+## Sublime Text 3
+alias subl="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
+
+# Variables
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
+# Custom plugins may be added to ~/.custom-omzsh/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+# Add wisely, as too many plugins slow down shell startup.
+plugins=(git k)
