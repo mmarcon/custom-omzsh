81f6391902d4ce06ed9587ed0f819ca2ada30c30
Massimiliano Marcon <me@marcon.me>
2016-10-07 11:49:39 +0200

    Turns out plugins can't be set here

--- overrides.zsh
+++ overrides.zsh
@@ -8,11 +8,3 @@ alias github='git -c user.name="Massimiliano Marcon" -c user.email="me@marcon.me
 
 ## Sublime Text 3
 alias subl="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
-
-# Variables
-
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
-# Custom plugins may be added to ~/.custom-omzsh/plugins/
-# Example format: plugins=(rails git textmate ruby lighthouse)
-# Add wisely, as too many plugins slow down shell startup.
-plugins=(git k macos)
