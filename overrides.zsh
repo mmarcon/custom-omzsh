c89e6dd62b5622aa242a86b7e6b25653ef6e1f6d
Massimiliano Marcon <me@marcon.me>
2017-10-24 10:13:01 +0200

    Adds npmr alias and gi() function

--- overrides.zsh
+++ overrides.zsh
@@ -3,8 +3,9 @@ alias zshconfig="subl ~/.zshrc"
 alias ohmyzsh="subl ~/.oh-my-zsh"
 alias zshreload="source ~/.zshrc"
 alias v="vagrant"
+alias npmr="npm run $@"
 
 alias github='git -c user.name="Massimiliano Marcon" -c user.email="me@marcon.me"'
 
-## Sublime Text 3
-alias subl="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
+## Functions
+function gi() { curl -L -s https://www.gitignore.io/api/$@ ;}
