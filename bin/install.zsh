4f49e36c2e0bc44f3e2806119516374d093df6b3
Massimiliano Marcon <me@marcon.me>
2018-08-19 17:19:22 +0200

    Install & config powerlevel9k

--- bin/install.zsh
+++ bin/install.zsh
@@ -1,6 +1,12 @@
 #!/usr/bin/env zsh
 
+# Nerd font
+brew update
+brew tap caskroom/fonts
+brew cask install font-hack-nerd-font
+
 plugins=$HOME/.custom-omzsh/plugins
+themes=$HOME/.custom-omzsh/themes
 
 # Create directories
 printf "creating directories => "
@@ -9,8 +15,7 @@ printf "done\n"
 
 
 # K (https://github.com/supercrabtree/k)
-
-k=$plugins/k 
+k=$plugins/k
 
 if [[ -d $k ]]; then
 	pushd $k
@@ -20,4 +25,16 @@ if [[ -d $k ]]; then
 else
 	printf "k is not installed, cloning from github => "
 	git clone https://github.com/supercrabtree/k $k &> /dev/null && printf "done\n" || "failed\n"
+fi
+
+# powerlevel9k (https://github.com/bhilburn/powerlevel9k.git)
+powerlevel9k=$themes/powerlevel9k
+if [[ -d $powerlevel9k ]]; then
+	pushd $powerlevel9k
+	printf "powerlevel9k is already installed, pulling latest => "
+	git pull
+	popd
+else
+	printf "powerlevel9k is not installed, cloning from github => "
+	git clone git clone https://github.com/bhilburn/powerlevel9k.git $powerlevel9k &> /dev/null && printf "done\n" || "failed\n"
 fi
--- overrides.zsh
+++ overrides.zsh
@@ -15,3 +15,19 @@ export PATH=$PATH:$GOPATH/bin
 ## Functions
 function gi() { curl -L -s https://www.gitignore.io/api/$@ ;}
 
+POWERLEVEL9K_MODE='nerdfont-complete'
+
+# Customise the Powerlevel9k prompts
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
+  custom_mongo dir vcs status
+)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
+    
+)
+POWERLEVEL9K_PROMPT_ADD_NEWLINE=true
+# Add the custom Mongo icon prompt segment
+POWERLEVEL9K_CUSTOM_MONGO="echo -n '\uE7A4'"
+POWERLEVEL9K_CUSTOM_MONGO_FOREGROUND="green"
+POWERLEVEL9K_CUSTOM_MONGO_BACKGROUND="black"
+
+source ~/.powerlevel9k/powerlevel9k.zsh-theme
