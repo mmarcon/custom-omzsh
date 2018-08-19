5db416d2604ca644c0ef903371ec2cc9a4a0bc63
Massimiliano Marcon <me@marcon.me>
2018-08-19 10:21:23 +0200

    Replaces subl with code

--- overrides.zsh
+++ overrides.zsh
@@ -1,10 +1,12 @@
 # Aliases
-alias zshconfig="subl ~/.zshrc"
-alias ohmyzsh="subl ~/.oh-my-zsh"
+alias zshconfig="code ~/.zshrc"
+alias ohmyzsh="code ~/.oh-my-zsh"
 alias zshreload="source ~/.zshrc"
 alias v="vagrant"
 
 alias github='git -c user.name="Massimiliano Marcon" -c user.email="me@marcon.me"'
 
-## Sublime Text 3
-alias subl="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
+#Go
+export GOPATH=$HOME/dev/go
+export PATH=$PATH:/usr/local/opt/go/libexec/bin
+export PATH=$PATH:$GOPATH/bin
