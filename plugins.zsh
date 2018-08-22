112ee0cae5cbe3448d02439eb432e7c8366e8270
Massimiliano Marcon <me@marcon.me>
2018-08-22 11:07:11 +0200

    Moves the plugins config to dedicated file
    This way the variable gets properly set  before
    oh-my-zsh is loaded.

---
+++ plugins.zsh
@@ -0,0 +1,5 @@
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
+# Custom plugins may be added to ~/.custom-omzsh/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+# Add wisely, as too many plugins slow down shell startup.
+plugins=(git k macos nvm apparix)
