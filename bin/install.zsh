a39f477fbf5ca427eca383ab3321eeb9dd3f3caa
Massimiliano Marcon <me@marcon.me>
2018-08-23 15:24:25 +0200

    Makes installing stuff optional

--- bin/install.zsh
+++ bin/install.zsh
@@ -5,17 +5,30 @@ plugins=$custom_omzsh/plugins
 themes=$custom_omzsh/themes
 system=$custom_omzsh/system
 
+source $custom_omzsh/helpers/functions.zsh
+
 # Oh-My-Zsh (https://github.com/robbyrussell/oh-my-zsh)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+if ask "Do you want to install Oh-My-Zsh?"; then
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
 
 # Brew stuff
 brew update
-brew tap caskroom/fonts
-brew cask install font-hack-nerd-font
-brew install apparix
+if ask "Do you want to install nerd-font?"; then
+    brew tap caskroom/fonts
+	brew cask install font-hack-nerd-font
+fi
+if ask "Do you want to install apparix?"; then
+	brew install apparix
+fi
+if ask "Do you want to install git?"; then
+	brew install git
+fi
 
 #Install NVM (https://github.com/creationix/nvm)
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+if ask "Do you want to install nvm?"; then
+	curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+fi
 
 # Create directories
 printf "creating directories => "
