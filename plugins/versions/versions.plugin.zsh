0518cc0fc53ec07916ec060e489f61270155a696
Massimiliano Marcon <me@marcon.me>
2018-08-23 13:37:38 +0200

    Adds plugin to check software versions

---
+++ plugins/versions/versions.plugin.zsh
@@ -0,0 +1,13 @@
+ver() {
+    # Mongo Shell
+    mongo_shell_version=$(mongo --version | egrep "version\sv" | cut -d " " -f 4 | sed s/v//)
+    mongo_shell_icon="\uE7A4"
+    mongo_shell_icon_color=$fg[green]
+    echo "${mongo_shell_icon_color}${mongo_shell_icon}${reset_color} Mongo Shell: ${mongo_shell_version}"
+
+    # Node
+    node_version=$(node --version | sed s/v//)
+    node_icon="\uf898"
+    node_icon_color=$fg[green]
+    echo "${node_icon_color}${node_icon}${reset_color} Node.js: ${node_version}"
+}
